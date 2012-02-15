e5db607c7f273d2a8a3d1b1198ad553741c1812f
Alexey <leopard.not.a@gmail.com>
2012-02-15 21:49:56 +0200

    table search

--- src/com/railsware/LibreOfficeCSVGenerator.java
+++ src/com/railsware/LibreOfficeCSVGenerator.java
@@ -4,29 +4,35 @@ import jargs.gnu.CmdLineParser;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 
 import ooo.connector.BootstrapSocketConnector;
 
-import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
 
 import com.sun.star.awt.XWindow;
 import com.sun.star.beans.PropertyValue;
 import com.sun.star.comp.helper.BootstrapException;
+import com.sun.star.container.XIndexAccess;
+import com.sun.star.container.XNameAccess;
 import com.sun.star.uno.Exception;
 import com.sun.star.uno.XComponentContext;
 import com.sun.star.frame.XComponentLoader;
 import com.sun.star.frame.XController;
 import com.sun.star.frame.XFrame;
 import com.sun.star.frame.XModel;
-import com.sun.star.frame.XStorable;
 import com.sun.star.lang.IllegalArgumentException;
+import com.sun.star.lang.IndexOutOfBoundsException;
+import com.sun.star.lang.WrappedTargetException;
 import com.sun.star.lang.XComponent;
 import com.sun.star.lang.XMultiComponentFactory;
+import com.sun.star.table.XCell;
+import com.sun.star.table.XCellRange;
 import com.sun.star.text.XTextDocument;
+import com.sun.star.text.XTextRange;
+import com.sun.star.text.XTextTablesSupplier;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.util.CloseVetoException;
-import com.sun.star.util.XReplaceDescriptor;
-import com.sun.star.util.XReplaceable;
 
 public class LibreOfficeCSVGenerator {
 	public enum DocFormat { 
@@ -139,10 +145,201 @@ public class LibreOfficeCSVGenerator {
         xComponentLoader = (XComponentLoader)
             UnoRuntime.queryInterface(XComponentLoader.class, desktop);
         
-        
-        generateDocByCSV(templateFile, csvFileValue, outputFolderValue, fileNameIndexValue, docFormat, verboseValue);
+        findTextsInCSV(templateFile, csvFileValue, outputFolderValue, fileNameIndexValue, docFormat, verboseValue);
 	}
 	
+	static private void findTextsInCSV(String templateFile, String csvFile, 
+			String outputFolder, Integer fileNameIndex, 
+			DocFormat docFormat, Boolean verboseMode){
+		try {
+			
+			String files;
+	        File folder = new File(outputFolder);
+	        File[] listOfFiles = folder.listFiles();
+	        CSVWriter writer = new CSVWriter(new FileWriter(outputFolder + File.separator + "out" + File.separator + "data.csv"), ',');
+	        writer.writeNext(new String[] { "Файл:", "Сума:" , "Платник:", "Місце проживання:", "Отримувач:", "Код:", "Розрахунковий рахунок:", "МФО банку:", "Призначення платежу:", "Призначення платежу 2:" });
+	        System.out.println("Files: " + listOfFiles.length);
+	        for (int i = 0; i < listOfFiles.length; i++) 
+	        {
+
+		         if (listOfFiles[i].isFile() && !listOfFiles[i].getName().startsWith(".")) 
+		         {
+		        	 files = listOfFiles[i].getName();
+		         } else {
+		        	 continue;
+		         }
+		         templateFile = outputFolder + File.separator + files;
+				
+				//open template
+				ArrayList<PropertyValue> props = new ArrayList<PropertyValue>(); 
+		        PropertyValue p = null; 
+		        if (templateFile != null) { 
+		            // Enable the use of a template document. 
+		            p = new PropertyValue(); 
+		            p.Name = "AsTemplate"; 
+		            p.Value = new Boolean (true); 
+		            props.add(p); 
+		        }  
+		        // Make the document initially invisible so the user does not 
+		        // have to watch it being built. 
+		        p = new PropertyValue(); 
+		        p.Name = "Hidden"; 
+		        p.Value = new Boolean(true); 
+		        props.add(p); 
+		        PropertyValue[] properties = new PropertyValue[props.size()]; 
+		        props.toArray(properties);
+		        
+		        if (verboseMode){
+		        	System.out.println( 
+                        "LibreOfficeCSVGenerator: Create the document based on template " + templateFile + "."); 
+		        }
+		        
+		        String templateFileURL = filePathToURL(templateFile); 
+		        XComponent document = xComponentLoader.loadComponentFromURL( 
+				        templateFileURL,    // URL of templateFile. 
+				        "_blank",           // Target frame name (_blank creates new frame). 
+				        0,                  // Search flags. 
+				        properties);
+		        
+		        // get window and frame
+		        XModel model = (XModel) UnoRuntime.queryInterface( 
+	                    XModel.class, document); 
+	            XController c = model.getCurrentController(); 
+	            XFrame frame = c.getFrame(); 
+	            XWindow window = frame.getContainerWindow();
+	            
+	            window.setEnable(false); 
+	            window.setVisible(false);
+		        
+		        // Create a Search Descriptor 
+		        XTextDocument xTextDocument = (XTextDocument)UnoRuntime.queryInterface(XTextDocument.class, document);
+		        
+		        
+		        // first query the XTextTablesSupplier interface from our document
+		        XTextTablesSupplier xTablesSupplier = (XTextTablesSupplier) UnoRuntime.queryInterface(
+		            XTextTablesSupplier.class, xTextDocument );
+		        // get the tables collection
+		        XNameAccess xNamedTables = xTablesSupplier.getTextTables();
+		        
+		        // now query the XIndexAccess from the tables collection
+		        XIndexAccess xIndexedTables = (XIndexAccess) UnoRuntime.queryInterface(
+		            XIndexAccess.class, xNamedTables);
+		        
+		        // get the tables
+//		        for (int j = 0; j < xIndexedTables.getCount(); j++) {
+		            Object table = xIndexedTables.getByIndex(0);
+		           
+		            XCellRange xCellRange = (XCellRange) UnoRuntime.queryInterface(
+		                    XCellRange.class, table );
+		           XCell xCell = xCellRange.getCellByPosition( 1, 1 );
+		           
+		           XTextRange xTextRange = (XTextRange) UnoRuntime.queryInterface(
+		        	         XTextRange.class, xCell );
+		           String mainString = xTextRange.getString();
+		           if (mainString.length() > 0){
+		        	   String[] tmpString = mainString.split("\n");
+		        	   for (int k=0;k<tmpString.length;k++){
+		        		   //System.out.println("data:" + tmpString[k]);
+		        	   }
+		           
+			           List<String> entries_csv = new ArrayList<String>();
+			           entries_csv.add(files);
+			           entries_csv.add(tmpString[2]);
+			           entries_csv.add(tmpString[4]);
+			           entries_csv.add(tmpString[6]);
+			           entries_csv.add(tmpString[8].replaceAll("Назва:", "").trim());
+			           
+			           String tmpStr = "";
+			           for (int nk=12;nk < 20;nk++){
+			        	   tmpStr += tmpString[nk];
+			           }
+			           entries_csv.add(tmpStr);
+			           tmpStr = "";
+			           for (int nk=21;nk < 35;nk++){
+			        	   tmpStr += tmpString[nk];
+			           }
+			           entries_csv.add(tmpStr);
+			           tmpStr = "";
+			           for (int nk=35;nk < 41;nk++){
+			        	   tmpStr += tmpString[nk];
+			           }
+			           entries_csv.add(tmpStr);
+			           String secAddr = "";
+			           for (int nk=0;nk < tmpString.length;nk++){
+			        	   if (tmpString[nk].compareToIgnoreCase("Призначення платежу:") == 0){
+			        		   secAddr = "";
+			        		   int nk2 = nk + 1;
+			        		   while(tmpString[nk2].compareToIgnoreCase("Платник:") != 0 || nk2 < 4){
+			        			   secAddr += tmpString[nk2];
+			        			   nk2++;
+			        		   }
+			        	   }
+			           }
+			           entries_csv.add(secAddr);
+			           
+			           // second
+			           XCell xCell2 = xCellRange.getCellByPosition( 1, 0 );
+			           
+			           XTextRange xTextRange2 = (XTextRange) UnoRuntime.queryInterface(
+			        	         XTextRange.class, xCell2 );
+			           String mainString2 = xTextRange2.getString();
+			           String[] tmpString2 = mainString2.split("\n");
+			           
+			           secAddr = "";
+			           for (int nk=0;nk < tmpString2.length;nk++){
+			        	   if (tmpString2[nk].compareToIgnoreCase("Призначення платежу:") == 0){
+			        		   secAddr = "";
+			        		   int nk2 = nk + 1;
+			        		   while(tmpString2[nk2].compareToIgnoreCase("Платник:") != 0 || nk2 < 4){
+			        			   secAddr += tmpString2[nk2];
+			        			   nk2++;
+			        		   }
+			        	   }
+			           }
+			           entries_csv.add(secAddr);
+			           
+			           writer.writeNext(entries_csv.toArray(new String[entries_csv.size()]));
+		           }
+//		        } 
+		        
+				//close doc
+		        
+				com.sun.star.util.XCloseable xCloseable = 
+	            	      (com.sun.star.util.XCloseable)UnoRuntime.queryInterface( 
+	            	        com.sun.star.util.XCloseable.class,model);
+				xCloseable.close(true);
+				
+			}
+	        
+	        writer.close();
+		
+		} catch (IllegalArgumentException e) {
+			System.err.println("ERROR: Error opening document");
+			e.printStackTrace();
+		} catch (IndexOutOfBoundsException e) {
+			// TODO Auto-generated catch block
+			System.err.println("ERROR: Exception open or save doc");
+			e.printStackTrace();
+		} catch (WrappedTargetException e) {
+			System.err.println("ERROR: Exception open or save doc");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CloseVetoException e) {
+			System.err.println("ERROR: Coudn't close document");
+			e.printStackTrace();
+		} catch (com.sun.star.io.IOException e) {
+			System.err.println("ERROR: Exception open or save doc");
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.err.println("ERROR: Exception open or save doc");
+			e.printStackTrace();
+		} finally {
+	        System.out.println("Work finished!");
+			System.exit(0);
+		}
+	}
+/*	
 	static private void generateDocByCSV(String templateFile, String csvFile, 
 			String outputFolder, Integer fileNameIndex, 
 			DocFormat docFormat, Boolean verboseMode){
@@ -312,7 +509,7 @@ public class LibreOfficeCSVGenerator {
 			System.exit(0);
 		}
 	}
-	
+*/	
 	 /** Convert a file path to URL format. */ 
     static private String filePathToURL(String file) { 
         File f = new File(file); 
